6904ce73595c4f7b6cace7a1bf88fda9306274d5
WahyuRohmanto <wahyurohmanto113@gmail.com>
2022-04-09 15:57:15 +0700

    pertemuan 5

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./componen/Navbar/navbar";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
